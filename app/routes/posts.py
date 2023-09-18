eac0430625ea7ed38a751876873df7848cef595e
Urvish <92674497+UrvishP4503@users.noreply.github.com>
2023-09-18 14:36:40 +0530

    get post and delete done using orm

--- app/routes/posts.py
+++ app/routes/posts.py
@@ -1,36 +1,53 @@
 from fastapi import Response, status, HTTPException, Depends, APIRouter
 from ..database import get_db
-from ..models import posts
+from ..models.posts import Post, NewPost
 from sqlalchemy.orm import Session
 
 router = APIRouter()
 
 
+@router.get("/post/all")
+async def get_all(db: Session = Depends(get_db)):
+    post = db.query(Post).all()
+    return {"message": post}
+
+
+@router.post("/post/new", status_code=status.HTTP_201_CREATED)
+async def make_new_post(post: NewPost, db: Session = Depends(get_db)):
+    new_post = Post(
+        title=post.title,
+        content=post.content,
+        published=post.published,
+    )
+
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+
+    return {"new post": new_post}
+
+
 @router.get("/post/{id}", status_code=status.HTTP_200_OK)
 async def get_post(id: int, db: Session = Depends(get_db)):
-    post = db.query(posts.Post).filter(posts.Post.id == id).first()
+    post = db.query(Post).filter(Post.id == id).first()
 
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Item not found.")
 
-    return {"post": post}
+    return Response(status_code=status.HTTP_200_OK, content={"post": post})
 
 
-@router.get("/post/all")
-async def get_all(db: Session = Depends(get_db)):
-    post = db.query(posts.Post).all()
-    return {"message": post}
+@router.delete("/post/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(Post).filter(Post.id == id)
 
+    if not post.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Item not found.")
 
-@router.post("/post/new", status_code=status.HTTP_201_CREATED)
-def creat_new_post(user_input: posts.NewPost, db: Session = Depends(get_db)):
-    new_post = posts.Post(
-        title=user_input.title,
-        content=user_input.content,
-        published=user_input.published
-    )
+    post.delete(synchronize_session=False)
 
-    db.add(new_post)
     db.commit()
-    return Response(status_code=status.HTTP_201_CREATED)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
