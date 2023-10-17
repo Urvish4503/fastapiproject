e45092c0751e767f7f1c372d3851df20cc22eebb
Urvish <92674497+UrvishP4503@users.noreply.github.com>
2023-10-17 20:54:00 +0530

    fixed the .env problem

--- app/database.py
+++ app/database.py
@@ -2,11 +2,9 @@ from typing import Generator, Any
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
-from .settings import settings
+from .settings import get_url
 
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://postgres:{settings.database_password}@localhost:5432/fastapi"
-)
+SQLALCHEMY_DATABASE_URL = get_url()
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/settings.py
+++ app/settings.py
@@ -1,18 +1,23 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
 
 
 class Settings(BaseSettings):
-    database_host: str = Field(..., env="DATABASE_HOST")
-    database_port: int
-    database_name: str
-    database_password: str
-    database_username: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
+    database_hostname: str = (
+        "The mystery of life isn't a problem to solve, but a reality to experience."
+    )
+    database_name: str = "First, solve the problem. Then, write the code."
+    database_password: str = "May You Rot In Hell."
+    database_username: str = "gandalf"
+    secret_key: str = "No"
+    algorithm: str = "Hardware eventually fails, but software lives forever."
+    database_port: int = 6969  # Nice
+    access_token_expire_minutes: int = 0
 
     model_config = SettingsConfigDict(env_file=".env")
 
 
 settings = Settings()
+
+
+def get_url() -> str:
+    return f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
